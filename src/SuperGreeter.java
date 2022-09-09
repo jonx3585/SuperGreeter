b0871131efc259b460923b2dc515c7538a4bcbbf
jonx3585 <jonx3585@stud.kea.dk>
2022-09-09 10:21:49 +0200

    added greet

--- src/SuperGreeter.java
+++ src/SuperGreeter.java
@@ -1,11 +1,18 @@
+import  java.util.Scanner;
 public class SuperGreeter {
 
     public static void main(String[] args) {
+        String name;
         SuperGreeter sg = new SuperGreeter();
         sg.start();
     }
     public void start() {
         System.out.println("Det virker!");
+    }
+
+    public void greet(){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
     }
 
 }
